149cef8400df15cfe25432fee5cd0058a573ee52
Digerati <cabal@digerati.design>
2023-06-29 19:48:30 +0100

    To preview SPA features for Dan @ Crewdentials

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,38 @@
-import { greetUser } from '$utils/greet';
+import barba from '@barba/core';
+import { restartWebflow } from '@finsweet/ts-utils';
+import { gsap } from 'gsap';
 
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+console.clear();
+console.log('BEFORE Barba Init');
+barba.init({
+  transitions: [
+    {
+      name: 'opacity-transition',
+      async leave(data) {
+        console.log('leave');
+        const wCurrent = document.querySelector('.w--current');
+        console.log(wCurrent);
+        wCurrent.classList.remove('w--current');
+        await gsap.to(data.current.container, {
+          opacity: 0,
+          duration: 0.3
+        });
+      },
+      async enter(data) {
+        console.log('enter');
+        data.next.container.classList.add('is-transitioning');
+        data.next.container.style.opacity = "0";
+        await gsap.to(data.next.container, {
+          opacity: 1,
+          duration: 0.3
+        });
+        data.next.container.classList.remove('is-transitioning');
+      }
+    },
+  ],
 });
+barba.hooks.after(() => {
+  console.log('after');
+  restartWebflow();
+});
+console.log('AFTER Barba Init');
